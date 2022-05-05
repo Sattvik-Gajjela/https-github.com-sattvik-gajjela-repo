fc4a3af518d15fcda213783ff12652a7be088fa6
Sattvik <89396335+Sattvik-Gajjela@users.noreply.github.com>
2022-05-05 08:05:54 -0500

    Update Interactionlayer.swift

--- Interactionlayer.swift
+++ Interactionlayer.swift
@@ -3,72 +3,69 @@ import Igis
 
 
 class InteractionLayer : Layer, KeyDownHandler
-{
-let ball = Ball()
-let paddleLeft = Paddle(rect:Rect(size:Size(width: 10, height: 100)))
-let paddleRight = Paddle(rect:Rect(size:Size(width: 10, height: 100)))
-let paddleTop = Paddle(rect:Rect(size:Size(width: 100,height: 10)))
-let paddleBottom = Paddle(rect:Rect(size:Size(width: 100,height: 10)))
-
-init()
 {   
-    super.init(name:"Interaction")
+    let ball = Ball()
+    let paddleLeft = Paddle(rect:Rect(size:Size(width: 10, height: 100)))
+    let paddleRight = Paddle(rect:Rect(size:Size(width: 10, height: 100)))
+    let paddleTop = Paddle(rect:Rect(size:Size(width: 100,height: 10)))
+    let paddleBottom = Paddle(rect:Rect(size:Size(width: 100,height: 10)))
 
-    insert(entity: ball, at .front)
-    ball.changeVelocity(velocityX: 3, velocityY: 3)
+    init()
+    {   
+        super.init(name:"Interaction")
 
-    insert(entity: paddleLeft, at .front)
-    insert(entity: oaddleRight, at .front)
-    insert(entity: paddleTop, at .front)
-    insert(entity: paddleBottom, at .front)
-}
+        insert(entity: ball, at: .front)
+        ball.changeVelocity(velocityX: 3, velocityY: 3)
 
-override func preSetup(canvasSize: Size, canvas: Canvas)
-{   
-    paddleLeft.move(to:Point(x: 100, y: 480))
-    paddleRight.move(to:Point(x: 1000, y: 1650))
-    paddleTop.move(to:Point(x: 500, y: canvasSize.center))
-    paddleBottom.move(to:Point(x: 500, y: 1000))
-    dispatcher.registerKeyDownHandler(handler: self)
-
-        let canvasBoundingRect = Rect(size:canvassSize)
-        let paddleLeftBoundingRect = Rect(topLeft:Point(x.paddleLeft.topLeft.x, y:paddleLeft.topLeft.y), size:Size(width:paddleLeft.size.width, height:paddleLeft.size.height))
-        paddleLeft.move(to:Point(x: 100, y: paddleLeftBoundingRect.topLeft.y-change))
+        insert(entity: paddleLeft, at: .front)
+        insert(entity: paddleRight, at: .front)
+        insert(entity: paddleTop, at: .front)
+        insert(entity: paddleBottom, at: .front)
     }
 
-func onKeyDown(key:String, code:String, ctrlKey:Bool, shiftKey:Bool, metaKey:Bool)
-{   
-    print(key)
-    let tlpl = paddleLeft.rectangle.rect.topLeft
-    let tlpr = paddleRight.rectangle.rect.topLeft
-    let tlpt = paddleTop.rectangle.rect.topLeft
-    let tlpo = paddleBottom.rectangle.rect.topLeft
-    switch key
-    {
-    case "w":
-        paddleLeft.move(to:Point(x: tlpl.x, y:tlpl.y - 15))
-    case "s":
-        paddleLeft.move(to:Point(x: tlpl.x, y:tlpl.y + 15))
-    case "[":
-        paddleRight.move(to:Point(x: tlpr.x, y:tlpr.y - 15))
-    case ";":
-        paddleRight.move(to:Point(x: tlpr.x, y:tlpr.y + 15))
-    case "c":
-        paddleBottom.move(to:Point(x: tlpo.x - 15, y:tlpr.y))
-    case "v":
-        paddleBottom.move(to:Point(x: tlpo.x + 15, y:tlpr.y))
-    case ",":
-        paddleTop.move(to:Point(x: tlpt.x - 15, y:tlpr.y))
-    case ".":
-        paddleTop.move(to:Point(x: tlpt.x + 15, y:tlpr.y))
-    default:
-        break
+    override func preSetup(canvasSize: Size, canvas: Canvas)
+    {   
+        paddleLeft.move(to:Point(x: 100, y: 480))
+        paddleRight.move(to:Point(x: 1000, y: 1650))
+        paddleTop.move(to:Point(x: 500, y: canvasSize.center))
+        paddleBottom.move(to:Point(x: 500, y: 1000))
+        dispatcher.registerKeyDownHandler(handler: self)
+
     }
-}
+
+    func onKeyDown(key:String, code:String, ctrlKey:Bool, shiftKey:Bool, metaKey:Bool)
+    {   
+        print(key)
+        let tlpl = paddleLeft.rectangle.rect.topLeft
+        let tlpr = paddleRight.rectangle.rect.topLeft
+        let tlpt = paddleTop.rectangle.rect.topLeft
+        let tlpo = paddleBottom.rectangle.rect.topLeft
+        switch key
+        {
+        case "w":
+            paddleLeft.move(to:Point(x: tlpl.x, y:tlpl.y - 15))
+        case "s":
+            paddleLeft.move(to:Point(x: tlpl.x, y:tlpl.y + 15))
+        case "p":
+            paddleRight.move(to:Point(x: tlpr.x, y:tlpr.y - 15))
+        case "l":
+            paddleRight.move(to:Point(x: tlpr.x, y:tlpr.y + 15))
+        case "c":
+            paddleBottom.move(to:Point(x: tlpo.x - 15, y:tlpr.y))
+        case "v":
+            paddleBottom.move(to:Point(x: tlpo.x + 15, y:tlpr.y))
+        case "n":
+            paddleTop.move(to:Point(x: tlpt.x - 15, y:tlpr.y))
+        case "m":
+            paddleTop.move(to:Point(x: tlpt.x + 15, y:tlpr.y))
+        default:
+            break
+        }
+    }
 
 
-override func postTearDown()
-{   
-    dispatcher.unregisterKeyDownHandler(handler:self)
-}
+    override func postTearDown()
+    {   
+        dispatcher.unregisterKeyDownHandler(handler: self)
+    }
 }
